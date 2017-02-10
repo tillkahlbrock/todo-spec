39e7f9847b4ff7f00c0944d24ecf00f5356f4a03
Till Kahlbrock <till@jimdo.com>
2017-02-10 11:31:15 +0100

    steps and specs for creating tasks

--- spec/features/step_definitions/task_steps.rb
+++ spec/features/step_definitions/task_steps.rb
@@ -5,3 +5,13 @@ end
 Then(/^I should see the empty list message$/) do
   expect(@taskListString.rstrip).to eq('Such empty...')
 end
+
+
+When(/^I create a task with the title "([^"]*)"$/) do |title|
+  cmd = "todo-cli add --title='#{title}'"
+  `#{cmd}`
+end
+
+Then(/^I should see a the task "([^"]*)"$/) do |title|
+  expect(@taskListString).to include(title)
+end
