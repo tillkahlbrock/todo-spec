f03c75fe33ef980b84bdb5c42abd201f9a7e7b8c
Till Kahlbrock <till@jimdo.com>
2017-02-12 19:31:59 +0100

    mark task as completed

--- spec/features/step_definitions/task_steps.rb
+++ spec/features/step_definitions/task_steps.rb
@@ -9,9 +9,24 @@ end
 
 When(/^I create a task with the title "([^"]*)"$/) do |title|
   cmd = "todo-cli add --title='#{title}'"
-  `#{cmd}`
+  id = `#{cmd}`
+  if @tasks_ids.nil?
+    @tasks_ids = {}
+  end
+  @tasks_ids[title] = id.strip
 end
 
 Then(/^I should see a the task "([^"]*)"$/) do |title|
   expect(@taskListString).to include(title)
+end
+
+And(/^I mark the task with the title "([^"]*)" as completed$/) do |title|
+  fail unless @tasks_ids.key? title
+  id = @tasks_ids[title]
+  cmd = "todo-cli complete --id='#{id}'"
+  `#{cmd}`
+end
+
+And(/^I should not see a the task "([^"]*)"$/) do |title|
+  expect(@taskListString).not_to include(title)
 end
